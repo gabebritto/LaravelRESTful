e440f284eddddfbbf21b5e36845475b07e18e575
Gabriel Souto <gabriel.souto@academico.ifpb.edu.br>
2023-06-23 17:38:23 -0300

    docs: Add documentation to auth routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,60 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
 class AuthController extends Controller
 {
-
+    /**
+     * Validates the received request and log in.
+     *
+     * @OA\Post(
+     *      path="/login",
+     *      operationId="login",
+     *      tags={"Auth"},
+     *      summary="Log in",
+     *      description="Returns the JWT token",
+     *  @OA\Parameter(
+     *      name="email",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *  @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="message",
+     *                  default="Successfully authenticated"
+     *              ),
+     *              @OA\Property(
+     *                  property="data",
+     *                  default="Token data json"
+     *              )
+     *          )
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Invalid Credentials",
+     *      )
+     * )
+     */
     public function login(Request $request)
     {
         $credentials = $request->only(['email', 'password']);
@@ -26,6 +76,47 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * Logout the token related user.
+     *
+     * @OA\Post(
+     *      path="/logout",
+     *      operationId="logout",
+     *      tags={"Auth"},
+     *      summary="Log out",
+     *      description="Returns a message related to action",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="message",
+     *                  default="Logged out successfully"
+     *              )
+     *          )
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Invalid Credentials",
+     *      ),
+     *     @OA\Response(
+     *          response="404",
+     *          description="Not authenticated",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="message",
+     *                  default="You are not logged in"
+     *              )
+     *          )
+     *     )
+     * )
+     */
     public function logout()
     {
         $authenticated = auth()->user();
